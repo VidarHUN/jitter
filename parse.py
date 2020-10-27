c65787e2630eab39e71c122e81b6625b451c0bf8
Richard Varadi <ricsi19981007@gmail.com>
2020-10-27 17:52:12 +0100

    Remove some junk code and add some comment

--- parse.py
+++ parse.py
@@ -1,16 +1,18 @@
 import sys
 import matplotlib.pyplot as plt
 
-def mean(l):
+# Calculate average of a list
+def avg(l):
     sum = 0
     for i in range(len(l)):
         sum += l[i]
     return sum / len(l)
 
-TICK = 1 / 8000
-base_ts = 0
+TICK = 1 / 8000 # Tick time 
+base_ts = 0 # First Timestamp
 data = []
 
+# Reading data from csv file
 with open(str(sys.argv[1]), 'r') as f: 
     line = f.readline()
     line = f.readline()
@@ -36,7 +38,6 @@ with open(str(sys.argv[1]), 'r') as f:
             int(new_line[-1].strip('"'))
             ])
 
-plot_data = []
 delta_forward = []
 delta_reverse = []
 
@@ -44,32 +45,30 @@ forward = data[0][0]
 reverse = data[1][0]
 
 for i in range(2, len(data) - 1):
-    if data[i][1] == '10.2.0.17':
-        # delta_forward.append(data[i][0] - forward)
+    if data[i][1] == sys.argv[2]:
+        # delta_forward.append(data[i][0] - forward) # Calculation of forward delta
         delta_forward.append(abs(((data[i][3] - base_ts) * TICK) - data[i][0]) * 1000)
         forward = data[i][0]
-    if data[i][1] == '10.2.0.11':
-        # delta_reverse.append(data[i][0] - reverse)
+    if data[i][1] == sys.argv[3]:
+        # delta_reverse.append(data[i][0] - reverse) # Calculation of reverse delta
         delta_reverse.append(abs(((data[i][3] - base_ts) * TICK) - data[i][0]) * 1000)
         reverse = data[i][0]
-    if data[i][4] == data[i+1][4]:
-        plot_data.append(data[i+1][0] - data[i][0])
-        jump = data[i+1][4]
         
 # print(*plot_data,sep='\n')
 # plt.plot(delta_forward, 'r', delta_reverse, 'g')
 
-plt.plot(delta_forward, label='Forward')  # etc.
-plt.plot(delta_reverse, label='Reverse')
+# Plot settings
+plt.plot(delta_forward, label='Forward')  # Set the forward stream data
+plt.plot(delta_reverse, label='Reverse')  # Set the reverse stream data
 plt.xlabel('Packets')
 plt.ylabel('Jitter (ms)')
 plt.title("Jitter")
 plt.legend()
-# plt.xlim(0, data[-1][0])
+
+# I don’t use it because I think it calculates an average of those values 
+# for every second. But I left it here to see if it would come in handy.
+# plt.xlim(0, data[-1][0]) # Change X axes range to 0 - last packet arrival time
 
 # print(*delta_forward, sep='\n')
-
-# print(mean(delta_forward))
-# print(mean(delta_reverse))
 
 plt.show()
